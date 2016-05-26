7a3e235041c79df7a4c259802443ffb67b18bd3b
Willi Schönborn <w.schoenborn@gmail.com>
2016-05-26 12:09:34 +0200

    Fixed typo

--- tracer-core/src/main/java/org/zalando/tracer/Generator.java
+++ tracer-core/src/main/java/org/zalando/tracer/Generator.java
@@ -26,7 +26,7 @@ import com.google.common.collect.ImmutableMap;
 import java.util.function.Function;
 
 /**
- * A generator is used to create a new trace identifier when a new lifecycle begins and now existing identifier exist.
+ * A generator is used to create a new trace identifier when a new lifecycle begins and no existing identifier exist.
  * This usually happens for incoming requests from external systems that are not aware of the special trace header or
  * if someone explicitly decides to start a new trace lifecycle without preserving existing identifiers.
  *
