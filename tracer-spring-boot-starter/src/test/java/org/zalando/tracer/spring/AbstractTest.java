7cd521b0ae896ccb7c5de6cf6f644aaadee68aa4
Willi Schönborn <willi.schoenborn@zalando.de>
2016-09-22 23:43:38 +0200

    Disabled parallel test execution due to race conditions

--- tracer-spring-boot-starter/src/test/java/org/zalando/tracer/spring/AbstractTest.java
+++ tracer-spring-boot-starter/src/test/java/org/zalando/tracer/spring/AbstractTest.java
@@ -3,15 +3,11 @@ package org.zalando.tracer.spring;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
-import static org.springframework.test.annotation.DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @Configuration
 @SpringBootTest(classes = Application.class)
-@DirtiesContext(classMode = AFTER_EACH_TEST_METHOD)
 public abstract class AbstractTest {
 
 }
