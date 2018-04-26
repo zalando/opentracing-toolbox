3bb07aa0c3579b41fa7f987b9c7a5befcd574956
Willi Schönborn <willi.schoenborn@zalando.de>
2018-04-26 16:25:59 +0200

    Incorporated feedback

--- tracer-core/src/main/java/org/zalando/tracer/FlowIDGenerator.java
+++ tracer-core/src/main/java/org/zalando/tracer/FlowIDGenerator.java
@@ -2,29 +2,26 @@ package org.zalando.tracer;
 
 import org.apiguardian.api.API;
 
-import java.nio.ByteBuffer;
 import java.util.Base64;
-import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
+import static java.nio.charset.StandardCharsets.US_ASCII;
 import static org.apiguardian.api.API.Status.STABLE;
 
 @API(status = STABLE)
 public final class FlowIDGenerator implements Generator {
 
-    private final Base64.Encoder encoder = Base64.getUrlEncoder();
+    private static final int LENGTH = 128 / Byte.SIZE;
+
+    private final Base64.Encoder encoder = Base64.getUrlEncoder().withoutPadding();
 
     @Override
     public String generate() {
-        final Random random = ThreadLocalRandom.current();
-
-        final ByteBuffer buffer = ByteBuffer.allocate(Long.BYTES * 2);
-        buffer.putLong(random.nextLong());
-        buffer.putLong(random.nextLong());
-
-        final byte[] bytes = buffer.array();
-
-        return "R" + encoder.encodeToString(bytes).replace("=", "").substring(1);
+        final byte[] bytes = new byte[LENGTH];
+        ThreadLocalRandom.current().nextBytes(bytes);
+        final byte[] encoded = encoder.encode(bytes);
+        encoded[0] = 'R';
+        return new String(encoded, US_ASCII);
     }
 
 }
