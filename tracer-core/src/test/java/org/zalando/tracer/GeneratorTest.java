b82780a9e0cb88c73881b43f8ff6249225c9a18f
Willi Schönborn <willi.schoenborn@zalando.de>
2018-04-29 00:33:28 +0200

    Restructured tests

--- tracer-core/src/test/java/org/zalando/tracer/GeneratorTest.java
+++ tracer-core/src/test/java/org/zalando/tracer/GeneratorTest.java
@@ -1,75 +1,45 @@
 package org.zalando.tracer;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.Arrays;
-import java.util.Iterator;
+import java.util.Collection;
+import java.util.regex.Pattern;
 
+import static java.util.regex.Pattern.compile;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.matchesPattern;
 import static org.hamcrest.Matchers.not;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class GeneratorTest {
 
-    @Test
-    void testUuid() {
-        final String value = new UUIDGenerator().generate();
-
-        assertThat(value, matchesPattern("[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}"));
+    static Collection<Arguments> generators() {
+        return Arrays.asList(
+                Arguments.of(new FlowIDGenerator(), compile("R[\\w-]{21}")),
+                Arguments.of(new PhraseGenerator(), compile("[a-z]+_[a-z]+_[a-z]+_[a-z]+_[a-z]+")),
+                Arguments.of(new Random64Generator(), compile("[a-f0-9]{16}")),
+                Arguments.of(new Random128Generator(), compile("[a-f0-9]{32}")),
+                Arguments.of(new UUIDGenerator(), compile("[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}"))
+        );
     }
 
-    @Test
-    void testFlowId() {
-        final String value = new FlowIDGenerator().generate();
-
-        assertThat(value, matchesPattern("R[\\w-]{21}"));
+    @ParameterizedTest
+    @MethodSource("generators")
+    void shouldMatchPattern(final Generator generator, final Pattern pattern) {
+        assertThat(generator.generate(), matchesPattern(pattern));
     }
 
-    @Test
-    void testPhrase() {
-        final String value = new PhraseGenerator().generate();
+    @ParameterizedTest
+    @MethodSource("generators")
+    void shouldGenerateRandomValues(final Generator generator) {
+        final String a = generator.generate();
+        final String b = generator.generate();
 
-        assertThat(value, matchesPattern("[a-z]+_[a-z]+_[a-z]+_[a-z]+_[a-z]+"));
+        assertThat(a, is(not(equalTo(b))));
     }
 
-    @Test
-    void testRandom64() {
-        final String value = new Random64Generator().generate();
-
-        assertThat(value, matchesPattern("[a-f0-9]{16}"));
-    }
-
-    @Test
-    void testRandom128() {
-        final String value = new Random128Generator().generate();
-
-        assertThat(value, matchesPattern("[a-f0-9]{32}"));
-    }
-
-    @Test
-    void testPhraseWozniakIsNotBoring() {
-        final Iterator<Integer> indexes = Arrays.asList(9, 143, 0, 0, 0, 0, 0, 0, 0, 0).iterator();
-
-        final String value = PhraseGenerator.generate(i -> indexes.next());
-
-        assertThat(value, is(not(containsString("boring_wozniak"))));
-    }
-
-    @Test
-    void testPhraseOver_1_000_000_000() {
-        assertTrue(PhraseGenerator.maxCombinations() > 1_000_000_000);
-    }
-
-    @Test
-    void testPhraseMinLength() {
-        assertThat(PhraseGenerator.minLength(), is(22));
-    }
-
-    @Test
-    void testPhraseMaxLength() {
-        assertThat(PhraseGenerator.maxLength(), is(61));
-    }
 }
