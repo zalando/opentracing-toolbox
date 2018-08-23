e19c44307efe7f6548ca0139a7ea837e6dfbb3e0
Jan Oopkaup <jan.oopkaup@nixis.de>
2018-08-23 08:47:20 +0200

    Reintegrate `TracerWebMvcAutoConfiguration` into main configuration class as per review comment
    No suitable testing scenario for this setup is available,
    because TracerAutoConfiguration implicitly imports
    TracerWebMvcAutoConfiguration.

--- tracer-spring-boot-starter/src/test/java/org/zalando/tracer/spring/FilterTest.java
+++ tracer-spring-boot-starter/src/test/java/org/zalando/tracer/spring/FilterTest.java
@@ -7,50 +7,23 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.FilterType;
-import org.springframework.core.SpringVersion;
-import org.springframework.test.context.BootstrapWith;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.util.ClassUtils;
-
-import javax.servlet.Filter;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.notNullValue;
-import static org.hamcrest.Matchers.nullValue;
 
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(classes = Application.class)
+@ImportAutoConfiguration(TracerAutoConfiguration.class)
 class FilterTest {
 
-    @ExtendWith(SpringExtension.class)
-    @SpringBootTest(classes = Application.class)
-    @ImportAutoConfiguration(TracerAutoConfiguration.class)
-    static class FilterNotOnClasspathTest {
+    @Autowired
+    @Qualifier("tracerFilter")
+    private FilterRegistrationBean tracerFilter;
 
-        @Autowired(required = false)
-        @Qualifier("tracerFilter")
-        private FilterRegistrationBean tracerFilter;
-
-        @Test
-        void shouldNotInitializeFilter() {
-            assertThat(tracerFilter, is(nullValue()));
-        }
-    }
-
-    @ExtendWith(SpringExtension.class)
-    @SpringBootTest(classes = Application.class)
-    @ImportAutoConfiguration(TracerWebMvcAutoConfiguration.class)
-    static class FilterOnClasspathTest {
-
-        @Autowired
-        @Qualifier("tracerFilter")
-        private FilterRegistrationBean tracerFilter;
-
-        @Test
-        void shouldInitializeFilter() {
-            assertThat(tracerFilter, is(notNullValue()));
-        }
+    @Test
+    void shouldInitializeFilter() {
+        assertThat(tracerFilter, is(notNullValue()));
     }
 }
