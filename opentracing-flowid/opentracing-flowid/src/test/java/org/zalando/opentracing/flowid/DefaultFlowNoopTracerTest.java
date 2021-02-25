035b658dc522833c7af3d4e031a20dc88c3cf0c1
Paul Ebermann <paul.ebermann@zalando.de>
2021-02-25 20:44:00 +0100

    issue #522 simplify test
    The NoopTracer doesn't need its span to be activated.

--- opentracing-flowid/opentracing-flowid/src/test/java/org/zalando/opentracing/flowid/DefaultFlowNoopTracerTest.java
+++ opentracing-flowid/opentracing-flowid/src/test/java/org/zalando/opentracing/flowid/DefaultFlowNoopTracerTest.java
@@ -1,13 +1,8 @@
 package org.zalando.opentracing.flowid;
 
-import io.opentracing.Scope;
-import io.opentracing.Span;
-import io.opentracing.mock.MockSpan;
-import io.opentracing.noop.NoopTracer;
 import io.opentracing.noop.NoopTracerFactory;
 import org.junit.jupiter.api.Test;
 
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -16,8 +11,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class DefaultFlowNoopTracerTest {
-    NoopTracer tracer = NoopTracerFactory.create();
-    private final Flow unit = Flow.create(tracer);
+    private final Flow unit = Flow.create(NoopTracerFactory.create());
 
     @Test
     void shouldThrowNoActiveSpanFoundException() {
@@ -26,14 +20,8 @@ class DefaultFlowNoopTracerTest {
 
     @Test
     void writeToShouldNotThrow() {
-        final Span span = tracer.buildSpan("test").start();
-
-        try (final Scope ignored = tracer.activateSpan(span)) {
-            final Map<String, String> target = new HashMap<>();
-
-            unit.writeTo(target::put);
-
-            assertEquals(emptyMap(), target);
-        }
+        final Map<String, String> target = new HashMap<>();
+        unit.writeTo(target::put);
+        assertEquals(emptyMap(), target);
     }
 }
