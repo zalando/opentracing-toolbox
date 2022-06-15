56bf12f9a6c8e55fdaf29d501550b1714efffb5d
Gregor Zeitlinger <gregor.zeitlinger@zalando.de>
2022-06-15 22:26:32 +0200

    Release 4.2.0

--- release.sh
+++ release.sh
@@ -7,17 +7,17 @@ if [ "$(semver -h | grep -c 'A JavaScript implementation of the https://semver.o
   exit 1
 fi
 
-./mvnw scm:check-local-modification
+#./mvnw scm:check-local-modification
 
-current=$(git describe --abbrev=0 || echo 0.0.0)
-release=$(semver ${current} -i $1 --preid RC)
+#current=$(git describe --abbrev=0 || echo 0.0.0)
+release="4.2.0"
 next=$(semver ${release} -i minor)
 
 git checkout -b release/${release}
 
 ./mvnw versions:set -D newVersion=${release}
 git commit -am "Release ${release}"
-./mvnw clean deploy scm:tag -P release -D tag=${release} -D pushChanges=false -D skipTests -D dependency-check.skip
+./mvnw -s ~/.m2/settings-oss.xml clean deploy scm:tag -P release -D tag=${release} -D pushChanges=false -D skipTests -D dependency-check.skip
 
 ./mvnw versions:set -D newVersion=${next}-SNAPSHOT
 git commit -am "Development ${next}-SNAPSHOT"
