3942c7a9fd6079e0d4a6539744f51f33ad793a08
Gregor Zeitlinger <gregor.zeitlinger@zalando.de>
2022-06-16 09:17:42 +0200

    prevent exception when a span is not present - usually because the filters are installed in the wrong order Tracing tools must never cause the application to fail.
    Signed-off-by: Gregor Zeitlinger <gregor.zeitlinger@zalando.de>

--- opentracing-flowid/opentracing-flowid/src/main/java/org/zalando/opentracing/flowid/DefaultFlow.java
+++ opentracing-flowid/opentracing-flowid/src/main/java/org/zalando/opentracing/flowid/DefaultFlow.java
@@ -1,19 +1,24 @@
 package org.zalando.opentracing.flowid;
 
-import io.opentracing.Span;
-import io.opentracing.Tracer;
-import lombok.AllArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-
-import javax.annotation.Nullable;
 import java.util.Optional;
 import java.util.function.BiConsumer;
 import java.util.function.BiFunction;
 import java.util.function.UnaryOperator;
 
+import javax.annotation.Nullable;
+
+import io.opentracing.Span;
+import io.opentracing.Tracer;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 @Slf4j
 @AllArgsConstructor
 final class DefaultFlow implements Flow {
+
+    private static final Logger LOG = LoggerFactory.getLogger(DefaultFlow.class.getName());
 
     private final Extractor extractor = new CompositeExtractor(
             new BaggageExtractor(),
@@ -26,24 +31,24 @@ final class DefaultFlow implements Flow {
 
     @Override
     public void readFrom(final UnaryOperator<String> reader) {
-        final Span span = activeSpan();
+        activeSpan().ifPresent(span -> {
+            final Optional<FlowId> now = extractor.extract(span, reader);
+            final Optional<FlowId> later = extractor.extract(span);
 
-        final Optional<FlowId> now = extractor.extract(span, reader);
-        final Optional<FlowId> later = extractor.extract(span);
+            final Optional<String> nowId = now.map(FlowId::getValue);
+            final Optional<String> laterId = later.map(FlowId::getValue);
 
-        final Optional<String> nowId = now.map(FlowId::getValue);
-        final Optional<String> laterId = later.map(FlowId::getValue);
-
-        if (nowId.equals(laterId)) {
-            later.ifPresent(flowId -> {
-                listener.onRead(span, flowId);
-            });
-        } else {
-            now.ifPresent(flowId -> {
-                bag(span, flowId);
-                listener.onRead(span, flowId);
-            });
-        }
+            if (nowId.equals(laterId)) {
+                later.ifPresent(flowId -> {
+                    listener.onRead(span, flowId);
+                });
+            } else {
+                now.ifPresent(flowId -> {
+                    bag(span, flowId);
+                    listener.onRead(span, flowId);
+                });
+            }
+        });
     }
 
     private void bag(final Span span, final FlowId flowId) {
@@ -52,14 +57,16 @@ final class DefaultFlow implements Flow {
 
     @Override
     public String currentId() {
-        return extractor.extract(activeSpan())
+        return activeSpan()
+                .flatMap(extractor::extract)
                 .map(FlowId::getValue)
-                .orElseThrow(IllegalStateException::new);
+                .orElse(null);
     }
 
     @Override
     public void writeTo(final BiConsumer<String, String> writer) {
-        extractor.extract(activeSpan())
+        activeSpan()
+                .flatMap(extractor::extract)
                 .map(FlowId::getValue)
                 .ifPresent(value ->
                         writer.accept(Header.FLOW_ID, value));
@@ -67,17 +74,21 @@ final class DefaultFlow implements Flow {
 
     @Override
     public <T> T write(final BiFunction<String, String, T> writer) {
-        return writer.apply(Header.FLOW_ID, currentId());
+        String currentId = currentId();
+        if (currentId != null) {
+            return writer.apply(Header.FLOW_ID, currentId);
+        }
+        return null;
     }
 
-    private Span activeSpan() {
+    private Optional<Span> activeSpan() {
         @Nullable final Span span = tracer.activeSpan();
 
         if (span == null) {
-            throw new IllegalStateException("No active span found");
+            LOG.warn("No active span found");
         }
 
-        return span;
+        return Optional.ofNullable(span);
     }
 
 }
